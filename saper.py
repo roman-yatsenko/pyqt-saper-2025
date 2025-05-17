6919b0e20936836dfb32a2a772dde4bc2c35ac70
YaRo <roman.yatsenko@hneu.net>
2025-05-17 15:51:24 +0300

    Add MainWindow class

---
+++ saper.py
@@ -0,0 +1,22 @@
+import random
+import time
+
+from PyQt6.QtGui import *
+from PyQt6.QtWidgets import *
+from PyQt6.QtCore import *
+
+
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Сапер")
+        self.setFixedSize(300, 300)
+        self.show()
+
+
+if __name__ == "__main__":
+    app = QApplication([])
+    window = MainWindow()
+    app.exec()
