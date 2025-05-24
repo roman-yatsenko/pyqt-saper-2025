4797ca479b1ee4b9a6a7002459326a3730b28052
YaRo <roman.yatsenko@hneu.net>
2025-05-24 15:49:40 +0300

    Add game statuses

--- saper.py
+++ saper.py
@@ -17,9 +17,22 @@ IMG_BOMB = QImage("./images/bomb.png")
 IMG_CLOCK = QImage("./images/clock.png")
 IMG_START = QImage("./images/rocket.png")
 
+STATUS_READY = 0
+STATUS_PLAY = 1
+STATUS_FAILED = 2
+STATUS_SUCCESS = 3
+
+STATUS_ICONS = {
+    STATUS_READY: "./images/plus.png",
+    STATUS_PLAY: "./images/smiley.png",
+    STATUS_FAILED: "./images/cross.png",
+    STATUS_SUCCESS: "./images/smiley-lol.png",
+}
+
 
 class Cell(QWidget):
     expandable = pyqtSignal(int, int)
+    clicked = pyqtSignal()
 
     def __init__(self, x, y):
         super().__init__()
@@ -80,6 +93,7 @@ class Cell(QWidget):
         self.update()
 
     def mouseReleaseEvent(self, event):
+        self.clicked.emit()
         if event.button() == Qt.MouseButton.LeftButton:
             self.click()
 
@@ -96,6 +110,7 @@ class MainWindow(QMainWindow):
         self.setFixedSize(300, 300)
         self.initUI()
         self.init_grid()
+        self.update_status(STATUS_READY)
         self.reset()
         self.setFixedSize(self.sizeHint())
         self.show()
@@ -152,6 +167,7 @@ class MainWindow(QMainWindow):
                 cell = Cell(x, y)
                 self.grid.addWidget(cell, x, y)
                 cell.expandable.connect(self.expand_reveal)
+                cell.clicked.connect(self.handle_click)
 
     def reset(self):
         self.mines_count = LEVELS[self.level][1]
@@ -219,6 +235,14 @@ class MainWindow(QMainWindow):
         for xi, yi, cell in self.get_around_cells(x, y):
             if not cell.is_mine and not cell.is_flagged and not cell.is_revealed:
                 yield (xi, yi, cell)
+
+    def update_status(self, status):
+        self.status = status
+        self.button.setIcon(QIcon(STATUS_ICONS[self.status]))
+
+    def handle_click(self):
+        if self.status == STATUS_READY:
+            self.update_status(STATUS_PLAY)
 
 
 if __name__ == "__main__":
