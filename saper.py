83bd11056d8d061b24163d16844c2e6f41f4cdb4
YaRo <roman.yatsenko@hneu.net>
2025-05-24 16:26:38 +0300

    Add flag handling

--- saper.py
+++ saper.py
@@ -35,6 +35,7 @@ class Cell(QWidget):
     expandable = pyqtSignal(int, int)
     clicked = pyqtSignal()
     game_over = pyqtSignal()
+    flagged = pyqtSignal(bool)
 
     def __init__(self, x, y):
         super().__init__()
@@ -113,6 +114,7 @@ class Cell(QWidget):
     def toggle_flag(self):
         self.is_flagged = not self.is_flagged
         self.update()
+        self.flagged.emit(self.is_flagged)
 
 
 class MainWindow(QMainWindow):
@@ -189,6 +191,7 @@ class MainWindow(QMainWindow):
                 cell.expandable.connect(self.expand_reveal)
                 cell.clicked.connect(self.handle_click)
                 cell.game_over.connect(self.game_over)
+                cell.flagged.connect(self.handle_flag)
 
     def reset(self):
         self.mines_count = LEVELS[self.level][1]
@@ -273,6 +276,10 @@ class MainWindow(QMainWindow):
 
     def game_over(self):
         self.update_status(STATUS_FAILED)
+
+    def handle_flag(self, flagged):
+        self.mines_count += -1 if flagged else +1
+        self.mines.setText(f"{self.mines_count:03d}")
 
 
 if __name__ == "__main__":
